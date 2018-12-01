5f2f7e6b0b5c381b1d42f2608a76ddc5c86c8726
Roiit2912 <rohit.rathi2912@gmail.com>
2018-12-01 17:31:47 +0530

    docker-compose,docker and entrypoint.sh file corrected

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 set -e
-run_cmd="dotnet run --server.urls http://*:80"
+# run_cmd="dotnet run --server.urls http://*:80"
+run_cmd="dotnet run"
 
 >&2 echo "Executing command"
 exec $run_cmd
